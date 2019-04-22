e5acc351a3455fd3e0d9e11ae0ee87a861adc989
xian <290455266@qq.com>
2019-04-22 21:06:04 +0800

    add

--- pointer.go
+++ pointer.go
@@ -15,6 +15,7 @@ func main(){
 	fmt.Println("initial:", i)
     zeroval(i)
 	fmt.Println("zeroval:", i)
+	//实参和行参都是类型 i，可以接受。此时在zeroval()中的 i只是 i的值拷贝，所以zeroval()的修改影响不到t1。
 	zeroptr(&i)
 	fmt.Println("zeroptr:", i)
 	fmt.Println("pointer:", &i)
